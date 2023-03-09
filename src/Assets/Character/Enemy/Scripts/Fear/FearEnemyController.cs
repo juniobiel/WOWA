3e7329adbc22e2f3bd6f0feeaa3c0f82af8a65af
Gabriel Junio <gabriel.junio@rocketmail.com>
2023-03-09 19:54:36 -0300

    Creating a echolocalization mechanic with iddle time

--- src/Assets/Character/Enemy/Scripts/Fear/FearEnemyController.cs
+++ src/Assets/Character/Enemy/Scripts/Fear/FearEnemyController.cs
@@ -7,11 +7,19 @@ namespace Assets.Character.Enemy.Scripts.Fear
         Rigidbody2D Rb;
 
         [SerializeField] bool IsRightPatrol;
+        [SerializeField] bool IsEcholocationActive;
+        [SerializeField] bool IsIddleActive;
+        [SerializeField] float timer;
+
+        GameObject playerObject;
 
         private void Start()
         {
             Rb = GetComponent<Rigidbody2D>();
+            playerObject = GameObject.FindGameObjectWithTag("Player");
             IsRightPatrol = false;   
+            IsEcholocationActive = false;
+            timer = 0;
         }
 
         private void FixedUpdate()
@@ -26,6 +34,10 @@ namespace Assets.Character.Enemy.Scripts.Fear
                 MoveEnemyLeft();
 
                 VerifyPatrolLeft();
+
+                Echolocatization();
+
+                IddleAnimation(IsRightPatrol);
             }
 
             else
@@ -33,6 +45,10 @@ namespace Assets.Character.Enemy.Scripts.Fear
                 MoveEnemyRight();
 
                 VerifyPatrolRight();
+
+                Echolocatization();
+
+                IddleAnimation(IsRightPatrol);
             }
 
         }
@@ -49,13 +65,54 @@ namespace Assets.Character.Enemy.Scripts.Fear
         }
         private void VerifyPatrolLeft()
         {
-            if (gameObject.transform.position.x <= PointsPatrol.InitialPoint.x)
-                IsRightPatrol = true;
+            if (gameObject.transform.position.x <= PointsPatrol.InitialPoint.x && !IsIddleActive)
+                IsEcholocationActive = true;
         }
         private void VerifyPatrolRight()
         {
-            if (gameObject.transform.position.x >= PointsPatrol.FinalPoint.x)
-                IsRightPatrol = false;
+            if (gameObject.transform.position.x >= PointsPatrol.FinalPoint.x && !IsIddleActive)
+                IsEcholocationActive = true;
+        }
+
+        private void Echolocatization()
+        {
+            if (IsEcholocationActive)
+            {
+                timer += Time.deltaTime;
+                if (timer <= 2f)
+                {
+                    float playerDistance = Vector3.Distance(gameObject.transform.position, playerObject.GetComponent<Transform>().position);
+
+                    Debug.Log("Echolocalizating");
+
+                    if (playerDistance <= 3)
+                    {
+                        Debug.Log("Attack the player");
+                    }
+                }
+                else
+                {
+                    IsEcholocationActive = false;
+                    IsIddleActive = true;
+                    timer -= timer;
+                }
+            }
+        }
+
+        private void IddleAnimation(bool isRightPatrol)
+        {
+            if (IsIddleActive)
+            {
+                timer += Time.deltaTime;
+                if (timer <= 2f)
+                    Debug.Log("Iddle Animation");
+                else
+                {
+                    IsIddleActive = false;
+                    IsRightPatrol = !isRightPatrol;
+                    timer -= timer;
+                }
+            }
         }
 
     }
